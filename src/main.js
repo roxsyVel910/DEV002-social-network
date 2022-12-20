b50d23b298b4ae3b78f157bac96d7b1f4dcc8eaa
Ana Cecilia Toribio Arrieta <cecitoribio@MacBook-Pro-de-Ana.local>
2022-12-20 16:27:18 -0500

    Agregue mensajes de errores en la pagina y archivo CSS

--- src/main.js
+++ src/main.js
@@ -45,6 +45,10 @@ const passwordRegister = document.querySelector("#passwordRegister");
 const passwordConfirm = document.querySelector("#passwordConfirm");
 const buttonRegister = document.querySelector("#register");
 const formRegister = document.querySelector("#formRegister");
+const messageEmail = document.querySelector("#messageEmail");
+const messagePassword = document.querySelector("#messagePassword");
+const anotherMessageEmail = document.querySelector("#anotherMessageEmail");
+const messagePasswordConfirm = document.querySelector("#messagePasswordConfirm");
 
 formRegister.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -56,9 +60,23 @@ const authFirebase = createUserWithEmailAndPassword(auth, emailRegister.value, p
         console.log(authFirebase)
     }) 
     .catch((error) => {
-        console.log(error)
+        console.log(error.message)
+        // identificardor unico para el error
+        console.log(error.code)
+
+        if(error.code === "auth/invalid-email"){
+            messageEmail.innerHTML = "correo invalido"
+        } else if (error.code === "auth/email-already-in-use"){
+            messageEmail.innerHTML = "el correo ya fue utilizado"
+        } else if (error.code === "auth/weak-password"){
+            messagePassword.innerHTML = "la contrasena debe tener por lo menos 6 caracteres"
+        }
     })
- 
-})
+    })
 
 
+    passwordConfirm.addEventListener("keydown", ()=> {
+        if( passwordConfirm.value != passwordRegister.value){
+            messagePasswordConfirm.innerHTML = "la contrasena no es igual"
+        }
+    })
