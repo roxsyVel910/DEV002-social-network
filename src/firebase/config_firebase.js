c0a1c8a0881ffad43afc9b71b83f7b6ba26d85e8
Ana Cecilia Toribio Arrieta <cecitoribio@MacBook-Pro-de-Ana.local>
2023-01-06 11:47:54 -0500

    modificacion de gitignore

---
+++ src/firebase/config_firebase.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAQxr8QueNyoazFnT3cCFo4YeCEz7S4FaE",
+    authDomain: "social-network-dog.firebaseapp.com",
+    projectId: "social-network-dog",
+    storageBucket: "social-network-dog.appspot.com",
+    messagingSenderId: "1073529346267",
+    appId: "1:1073529346267:web:b2b1e7b435a9c16adc14fe",
+    measurementId: "G-5PBN6ZD4GQ"
+};
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,3 +1,5 @@
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
@@ -8,16 +10,8 @@ import {getAuth} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.j
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Firebase, esta informacion la utiliza firebase para el uso de su servicio
-const firebaseConfig = {
-    apiKey: "AIzaSyAQxr8QueNyoazFnT3cCFo4YeCEz7S4FaE",
-    authDomain: "social-network-dog.firebaseapp.com",
-    projectId: "social-network-dog",
-    storageBucket: "social-network-dog.appspot.com",
-    messagingSenderId: "1073529346267",
-    appId: "1:1073529346267:web:b2b1e7b435a9c16adc14fe",
-    measurementId: "G-5PBN6ZD4GQ"
-};
 
+import { firebaseConfig } from "./config_firebase.js";
 
 
 // Initialize Firebase
